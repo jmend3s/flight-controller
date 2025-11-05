f0bf85e47e6ddd2319576a46e8c727f9b680404b
Jmendes42 <jmendes.3595@gmail.com>
2025-11-05 17:02:01 +0000

    Starting on SPI abstraction. Really tired of config stuff...

---
+++ zephyr_ws/drivers/spi/SpiDevice.h
@@ -0,0 +1,17 @@
+
+#ifndef __SPI_DEVICE_H__
+#define __SPI_DEVICE_H__
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/spi.h>
+#include <zephyr/sys/printk.h>
+
+class SpiDevice
+{
+public:
+    // SpiDevice();
+
+private:
+};
+
+#endif

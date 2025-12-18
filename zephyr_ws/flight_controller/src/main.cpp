054c36526f5f5229a2da5b5660ae22fb9fb12603
Jmendes42 <jmendes.3595@gmail.com>
2025-12-18 07:13:15 +0000

    ADC is working

--- zephyr_ws/flight_controller/src/main.cpp
+++ zephyr_ws/flight_controller/src/main.cpp
@@ -1,6 +1,28 @@
 
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/adc.h>
+
+static const adc_dt_spec adcChannel = ADC_DT_SPEC_GET(DT_PATH(zephyr_user));
 
 int main()
 {
+    adc_is_ready_dt(&adcChannel);
+    adc_channel_setup_dt(&adcChannel);
 
+    int16_t buf = 0;
+    adc_sequence sequence
+    {
+        .buffer = &buf,
+        .buffer_size = sizeof(buf)
+    };
+
+    adc_sequence_init_dt(&adcChannel, &sequence);
+
+    while (1)
+    {
+        adc_read_dt(&adcChannel, &sequence);
+        int32_t const value = buf;
+        printk("value = %ld\n", value);
+        k_sleep(K_MSEC(500));
+    }
 }

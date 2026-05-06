32588827752fa0c4579dc1b3206896a3f690a591
Jmendes42 <jmendes.3595@gmail.com>
2026-05-06 17:35:48 +0100

    IMU initialization failing, I think it can be an hardware problen

--- zephyr_ws/flight_controller/src/application/Application.h
+++ zephyr_ws/flight_controller/src/application/Application.h
@@ -6,11 +6,15 @@
 #include "BatteryLedService.h"
 #include "EscDriver.h"
 
+#include "imu/Mpu6050.h"
+#include "ImuService.h"
+
 #include "Component.h"
 #include "TimeSource.h"
 
 #include "Adc.h"
 #include "Gpio.h"
+#include "I2c.h"
 #include "Pwm.h"
 
 #include <cstdlib>
@@ -19,19 +23,23 @@
 class Application
 {
 public:
-    Application(AdcSpec& adc, GpioSpec& greenLed, GpioSpec& yellowLed, GpioSpec& redLed, PwmSpec& esc1)
+    Application(AdcSpec& adc, GpioSpec& greenLed, GpioSpec& yellowLed, GpioSpec& redLed, I2cSpec& imuSpec, PwmSpec& esc1)
         : _adc(adc)
         , _greenLed(greenLed, GpioMode::Output)
         , _yellowLed(yellowLed, GpioMode::Output)
         , _redLed(redLed, GpioMode::Output)
+        , _imuI2c(imuSpec)
         , _esc1(esc1)
         , _batteryMonitoringService(_adc, _state)
         , _batteryLedService(_greenLed, _yellowLed, _redLed, _state)
+        , _mpu6050(_imuI2c)
+        , _mpu6050Service(_mpu6050)
         , _escDriver(_esc1)
     {
         _components[0] = &_batteryMonitoringService;
         _components[1] = &_batteryLedService;
-        _components[2] = &_escDriver;
+        _components[2] = &_mpu6050Service;
+        _components[3] = &_escDriver;
     }
 
     Component** components()
@@ -59,17 +67,21 @@ private:
     Gpio _greenLed;
     Gpio _yellowLed;
     Gpio _redLed;
+    I2c _imuI2c;
     Pwm _esc1;
 
     BatteryMonitoringService _batteryMonitoringService;
     BatteryLedService _batteryLedService;
     BatteryState _state;
 
+    Mpu6050 _mpu6050;
+    ImuService _mpu6050Service;
+
     EscDriver _escDriver;
 
-    size_t const _count = 3;
-    uint64_t _tickStorage[3];
-    Component* _components[3];
+    size_t const _count = 4;
+    uint64_t _tickStorage[4];
+    Component* _components[4];
 
     TimeSource _time;
 };
